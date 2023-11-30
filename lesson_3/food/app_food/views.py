6c10e801d335c1e0203270cf6d2ca9b4d280aa4f
kamil <kotlimetov@gmail.com>
2023-11-30 21:40:13 +0300

    lesson3.1_done

--- lesson_3/food/app_food/views.py
+++ lesson_3/food/app_food/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
-from .models import Product
+from django.http import HttpResponse
+from .models import Product, Category
 
-def products_list(request, *args, **kwargs):
-    if request.method == 'GET':
-        products = Product.objects.all()
-        # print(products)
-        context = {'products': products}
-        return render(request, template_name='app_food/index.html', context=context)
+# def products_list(request, *args, **kwargs):
+#     if request.method == 'GET':
+#         products = Product.objects.all()
+#         # print(products)
+#         context = {'products': products}
+#         return render(request, template_name='app_food/index.html', context=context)
+
+def main(request, *args, **kwargs):
+    return render(request, 'app_food/base.html')
+
+def catalog(request, *args, **kwargs):
+    categories = Category.objects.all()
+    return render(request, 'app_food/index.html', context={'categories': categories})
+
+def category(request, *args, **kwargs):
+    products = Product.objects.filter(category__id=kwargs['pk'])
+    return render(request, 'app_food/category.html', context={'products': products})
