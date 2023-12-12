8f333dd7c992ac89a06f82ecd46807a117e5b568
Б - 8 < - 8@DESKTOP-RM5M6F9>
2023-12-12 21:39:32 +0500

    lessons3.6_done

--- lesson_3/food/blog/forms.py
+++ lesson_3/food/blog/forms.py
@@ -1,10 +1,24 @@
 from django import forms
-from .models import News
+from .models import News, Comment
 
-class NewsForm(forms.ModelForm):
+# class NewsForm(forms.ModelForm):
+#
+#
+#     class Meta:
+#         model = News
+#         # fields = '__all__'
+#         exclude = ['activity_flag']
 
+class PostForm(forms.Form):
+    title = forms.CharField(max_length=50, label='Заголовок Новости')
+    content = forms.CharField(widget=forms.Textarea, label='Содержание')
+    username = forms.CharField(max_length=20, label='Логин')
+    first_name = forms.CharField(max_length=20, label='Имя')
+    last_name = forms.CharField(max_length=20, label='Фамилия')
+    email = forms.EmailField(max_length=50, label='email')
+
+class CommentForm(forms.ModelForm):
 
     class Meta:
-        model = News
-        # fields = '__all__'
-        exclude = ['activity_flag']
+        model = Comment
+        exclude = ['news', 'published_at']
--- lesson_3/food/blog/urls.py
+++ lesson_3/food/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import NewsListView, RegisterNews
+from .views import NewsListView, RegisterNews, NewsDetailView
 
 urlpatterns = [
     path('news_list/', NewsListView.as_view(), name='news_list'),
-    path('new_news/', RegisterNews.as_view(), name='register_news')
+    path('new_news/', RegisterNews.as_view(), name='register_news'),
+    path('news_list/<int:pk>/', NewsDetailView.as_view(), name='news_detail')
 ]
